178b95fb5acf09858d372bae1670bd4e61ba5783
CarolineGrandval <caroline.grandval2021@campus-eni.fr>
2021-06-21 14:39:12 +0200

    jsp accueil

--- src/fr/eni/servlets/Accueil.java
+++ src/fr/eni/servlets/Accueil.java
@@ -3,14 +3,17 @@ package fr.eni.servlets;
 import org.eclipse.jdt.internal.compiler.env.ISourceType;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/accueil")
 public class Accueil extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("coucou");
+        req.getRequestDispatcher("WEB-INF/accueil.jsp").forward(req, resp);
     }
 }
---
+++ web/WEB-INF/accueil.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: cgrandval2021
+  Date: 21/06/2021
+  Time: 14:37
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Accueil</title>
+</head>
+<body>
+Pade d'accueil
+</body>
+</html>
